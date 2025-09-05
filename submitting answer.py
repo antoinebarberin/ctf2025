69ae72dcdd419d50c1e56a4607c9f9c74d186180
Antoine Barberin <antoine.barberin@student.isae-supaero.fr>
2025-09-05 14:53:11 +0200

    back up save

--- submitting answer.py
+++ submitting answer.py
@@ -1,7 +1,7 @@
 import requests
 
-data = {"number": "85",
-        "answer": "3",
+data = {"number": "167",
+        "answer": "float64",
         "user": "a.barberin"}
 r = requests.post("http://34.163.196.38/", data=data)
 print(r.text)
